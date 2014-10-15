96c424dbe44204870c9308d6d9b99b7713049490
chelm <christopher.helm@gmail.com>
2014-10-15 11:04:22 -0600

    new pattern for controllers

--- controller/index.js
+++ controller/index.js
@@ -1,23 +1,25 @@
 var crypto = require('crypto'),
-  BaseController = require('koop-server/lib/Controller.js'),  
+  BaseController = require('koop-server/lib/BaseController.js'),  
   fs = require('fs');
 
 var Controller = function( Elasticsearch ){
 
+  var controller = {};
+  controller.__proto__ = BaseController();
+
   // general helper for not found repos
-  this.notFound = function(req, res){
+  controller.notFound = function(req, res){
     res.send('A useful error for missing data', 404);
   };
   
-  
   // general helper for error'd requests
-  this.Error = function(req, res){
+  controller.Error = function(req, res){
     res.send('Another useful error', 500);
   };
   
   
   // 
-  this.get = function(req, res){
+  controller.get = function(req, res){
       var key = ['Elasticsearch'];
       Elasticsearch.find(req.params.id, req.params.dataset, req.query, function(err, data){
         if (err){
@@ -28,7 +30,7 @@ var Controller = function( Elasticsearch ){
       });
   };
   
-  this.featureservice = function(req, res){
+  controller.featureservice = function(req, res){
       var callback = req.query.callback, self = this;
       delete req.query.callback;
   
@@ -37,16 +39,16 @@ var Controller = function( Elasticsearch ){
           res.send(err, 500);
         } else {
           delete req.query.geometry;
-          BaseController._processFeatureServer( req, res, err, data, callback);
+          controller.processFeatureServer( req, res, err, data, callback);
         }
       });
   };
   
-  this.preview = function(req, res){
+  controller.preview = function(req, res){
       res.render(__dirname + '/../views/index', { locals:{ id: req.params.id, dataset: req.params.dataset } });
   };
 
-  return this;
+  return controller;
 
 };
 
